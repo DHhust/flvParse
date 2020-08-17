091d077f0c319b253457f27ef1c10d571b6f1902
huadong <huadong@yy.com>
2020-08-17 21:28:43 +0800

    first commit

---
+++ FLVParse/flvParse.cpp
@@ -0,0 +1,7 @@
+#include "flvParse.h"
+#include <stdio.h>
+
+void main(int argc, char* argv[]) {
+	FILE* p_flvFile;
+	if()
+}
---
+++ FLVParse/flvParse.h
@@ -0,0 +1,24 @@
+#ifndef _FLV_PARSE_H
+#define _FLV_PARSE_H
+
+typedef struct
+{
+	unsigned char signature[3];
+	unsigned char version;
+	unsigned char flvInformation;
+	unsigned int headerLength;
+}flvHeader, *p_flvHeader;
+
+
+typedef struct
+{
+	unsigned char tagType;
+	unsigned char timeStamp[3];
+	unsigned char timeStampExt;
+	unsigned char streamID[3];
+}tagHeader, *p_tagHeader;
+
+
+
+#endif // !_FLV_PARSE_H
+
