97aedb3b62ee38fb233ea71d154035c89cc5ba0e
hua dong <920836417@qq.com>
2020-08-18 00:06:07 +0800

    feat: flv header reader

--- FLVParse/flvParse.cpp
+++ FLVParse/flvParse.cpp
@@ -1,7 +1,22 @@
 #include "flvParse.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-void main(int argc, char* argv[]) {
-	FILE* p_flvFile;
+int read_flv_header(FILE* p_flvFile, p_flvHeader file_header){
+	int ret=0;
+	file_header=malloc(sizeof(flvHeader))
+	if(file_header==NULL){
+		fprintf(stderr,"malloc file header failed\n")
+		perror("")
+	}
+}
+
+int main(int argc, char* argv[]) {
+	FILE* p_flvFile=NULL;
+	if((p_flvFile = fopen("./sample.flv", "rb"))==NULL){
+		perror("flv file open failed!")
+		return;
+	}
+	p_flvHeader file_header=NULL;
 	if()
 }
